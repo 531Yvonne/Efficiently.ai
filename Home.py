1f23bbd1b6c534937eceaf64be8ce5485d024996
yvesyang <yangyanyves@gmail.com>
2023-11-13 17:44:15 -0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,7 +2,14 @@ import streamlit as st
 
 st.set_page_config(page_title="Efficiently.ai", page_icon="🌠")
 
-st.title("Welcome to Efficiently.ai\n where find efficiency to the next level!")
+st.title("Welcome to Efficiently.ai")
+st.caption("🌠 Boost your efficiency to the next level!")
+"💬 Customized Chatbot: easy export and language settings"
+"🗂️ Summarize complex files and answer your questions"
+"🎬 Summarize videos and answer your questions"
+"🛜 What's on the webpage?"
+"📊 Generate easy charts based on given data"
+
 
 if "openai_api_key" not in st.session_state:
     st.session_state["openai_api_key"] = ""
@@ -13,3 +20,5 @@ submit = st.button("Submit")
 if submit:
     st.session_state["openai_api_key"] = openai_api_key
     st.write("You have entered your key")
+    with open('.env', 'w') as env_file:
+        env_file.write(f'OPENAI_API_KEY={openai_api_key}\n')
